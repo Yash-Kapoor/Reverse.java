f9c224fdf755f9980550cbc7c2f13da3fa0a1df3
yashk <yashk@10.12.91.20>
2024-02-07 09:34:32 +0530

    My First Commit

---
+++ Reverse.java/src/Day1/Assignment1.java
@@ -0,0 +1,20 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scan.nextInt();
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println("Reversed number: " + reversed);
+    }
+}
